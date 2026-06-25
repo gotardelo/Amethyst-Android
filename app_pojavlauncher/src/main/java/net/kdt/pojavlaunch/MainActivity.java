d17b73d9c12dfdaf11754bab00e55d292ce53164
beletatogui <nelsonbeletato@gmail.com>
2026-06-25 19:33:07 -0300

    Prepare runtime before mobile launch

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/MainActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/MainActivity.java
@@ -469,10 +469,14 @@ public class MainActivity extends BaseActivity implements ControlButtonMenuListe
         if(Tools.LOCAL_RENDERER.equals("opengles_mobileglues")) {
             LauncherPreferences.writeMGRendererSettings();
         }
-        JREUtils.redirectAndPrintJRELog();
-        LauncherProfiles.load();
         int requiredJavaVersion = 8;
         if(version.javaVersion != null) requiredJavaVersion = version.javaVersion.majorVersion;
+        if (!NewJREUtil.installNewJreIfNeeded(this, version)) {
+            Tools.runOnUiThread(()-> mServiceBinder.isActive = false);
+            return;
+        }
+        JREUtils.redirectAndPrintJRELog();
+        LauncherProfiles.load();
         Tools.launchMinecraft(this, minecraftAccount, minecraftProfile, versionId, requiredJavaVersion);
         //Note that we actually stall in the above function, even if the game crashes. But let's be safe.
         Tools.runOnUiThread(()-> mServiceBinder.isActive = false);
